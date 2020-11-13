1c01921db1c6cc956bbed4f599afa1d41ed1e32f
조슬지 <seulji@joseuljiui-iMac.local>
2020-11-13 23:26:47 +0900

    [py]201113

---
+++ 2_IF_problem/2.py
@@ -0,0 +1,12 @@
+score = int(input())
+
+if 100 >= score and score >= 90:
+    print('A')
+elif 90 > score and score >= 80:
+    print('B')
+elif 80 > score and score >= 70:
+    print('C')
+elif 70 > score and score >= 60:
+    print('D')
+else:
+    print('F')
